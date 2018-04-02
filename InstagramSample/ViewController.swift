8082af117ef1bf38c91d74bf480ffc6959c99048
thomasinyvr <tomfriesman@gmail.com>
2018-04-02 08:07:58 -0700

    Initial Commit

---
+++ InstagramSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramSample
+//
+//  Created by Thomas Friesman on 2018-04-02.
+//  Copyright © 2018 Thomas Friesman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
